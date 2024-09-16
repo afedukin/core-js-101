861733f177a48b8aca96a5850114195af80c6fed
afedukin <fedukin.aa@yandex.ru>
2024-09-16 15:21:10 +0400

    feat: 04 solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -577,8 +577,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middleIndex = Math.floor(arr.length / 2);
+  return arr.length % 2 === 0
+    ? [...arr.slice(middleIndex), ...arr.slice(0, middleIndex)]
+    : [...arr.slice(middleIndex + 1), arr[middleIndex], ...arr.slice(0, middleIndex)];
 }
 
 module.exports = {
